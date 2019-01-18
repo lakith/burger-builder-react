bd0126da84317465f923e7f78f8673de4934f6ce
lakith <lakith1995@gmail.com>
2019-01-19 01:20:29 +0530

    improving performance

--- src/components/Burger/OrderSummery/OrderSummery.js
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummery = (props) =>{
+class OrderSummery extends Component {
 
-    const ingrediants = Object.keys(props.ingrediants)
-                        .map(bgKey => (
-                            <li key={bgKey}>
-                                <span style={{textTransform:'capitalize'}}> : {bgKey}</span>
-                                {props.ingrediants[bgKey]}
-                            </li>
-                        ));
+    componentWillUpdate(){
+        console.log("[Order Summery] Component Will Update");
+    }
 
-    return (
-        <Auxiliary>
-                <h3>Your Order</h3>
-                <p>A Delicious burger with following ingrediants</p>
-                <ul>
-                    {ingrediants}
-                </ul>
-                <p> <strong>Total Price : {props.price} /=</strong></p>
-                <p>Continue to checkout</p>
-                <Button btnType ="Danger" clicked={props.purchaseCanceled}>Cansel</Button>
-                <Button btnType ="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </Auxiliary>
-    );
+    render(){
+        const ingrediants = Object.keys(this.props.ingrediants)
+                            .map(bgKey => (
+                                <li key={bgKey}>
+                                    <span style={{textTransform:'capitalize'}}> : {bgKey}</span>
+                                    {this.props.ingrediants[bgKey]}
+                                </li>
+                            ));
+
+        return (
+            <Auxiliary>
+                    <h3>Your Order</h3>
+                    <p>A Delicious burger with following ingrediants</p>
+                    <ul>
+                        {ingrediants}
+                    </ul>
+                    <p> <strong>Total Price : {this.props.price} /=</strong></p>
+                    <p>Continue to checkout</p>
+                    <Button btnType ="Danger" clicked={this.props.purchaseCanceled}>Cansel</Button>
+                    <Button btnType ="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Auxiliary>
+        );
+    }
 }
 
-export default orderSummery;
+export default OrderSummery;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import BackDrop from '../Backdrop/BackDrop';
 
-const modal = (props) =>{
+class Modal extends Component {
+
+
+    shouldComponentUpdate (nextProps,nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Moldal] Will Updates');
+    }
+
+    render(){
    
     return(
-        <Auxiliary>
-            <BackDrop show = {props.show} clicked = {props.modelClosed}/>
-            <div 
-            className={classes.Modal}
-            style={{transform:props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity:props.show ? '1' : '0'}}
-            >
-                {props.children}
-            </div>
-        </Auxiliary>
-    )
+            <Auxiliary>
+                <BackDrop show = {this.props.show} clicked = {this.props.modelClosed}/>
+                <div 
+                className={classes.Modal}
+                style={{transform:this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity:this.props.show ? '1' : '0'}}
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        )
+    }
 }
 
-export default modal;
+export default Modal;
